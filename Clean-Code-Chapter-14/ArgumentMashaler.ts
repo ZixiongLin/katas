08e424d10950d9cd0fc5fdc868e34e2a5ac6e546
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2024-07-25 20:36:14 +0100

    Created ArgumentMashler

---
+++ Clean-Code-Chapter-14/ArgumentMashaler.ts
@@ -0,0 +1,6 @@
+export class ArgumentMashaler{}
+
+export class BooleanArgumentMashaler extends ArgumentMashaler{}
+
+export class StringArgumentMashaler extends ArgumentMashaler{}
+export class IntegerArgumentMashaler extends ArgumentMashaler{}
